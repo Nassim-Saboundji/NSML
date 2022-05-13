0bfba7913c45c66fdbed7c577d9cff390cfea987
Nassim's Software <70408318+Nassim-Saboundji@users.noreply.github.com>
2022-05-12 23:18:44 -0400

    working on conversion to html

--- src/nsml-parser.py
+++ src/nsml-parser.py
@@ -1,5 +1,6 @@
 import enum
 import sys
+from html.parser import HTMLParser
 
 args = sys.argv
 
@@ -32,10 +33,6 @@ for i, tag in enumerate(tags):
         tags[i] = tags[i-1]
 
 
-print(brackets)
-print(tags)
-
-
 hierarchy = "".join(brackets)
 hierarchy = hierarchy.replace("{}", "c")
 
@@ -43,9 +40,6 @@ nb_brackets = 0
 for c in hierarchy:
     if c == "}" or c == "{":
         nb_brackets += 1
-
-print(nb_brackets)
-print(hierarchy)
 
 final_hierarchy = []
 for c in hierarchy:
@@ -59,6 +53,46 @@ for c in hierarchy:
     if c == "c":
         final_hierarchy.append("c")
     
+right_side = []
+left_side = []
+for i, position in enumerate(final_hierarchy):
+    if position == "p":
+        right_side.append("<"+tags[i]+">")
+        left_side.append("</"+tags[i]+">")
+    if position == "c":
+        right_side.append("<"+tags[i]+">")
+        right_side.append("</"+tags[i]+">")
 
-print(final_hierarchy)
 
+DOM_structure = right_side + list(reversed(left_side))
+
+#print(DOM_structure)
+
+class Parser(HTMLParser):
+  # method to append the start tag to the list start_tags.
+  def handle_starttag(self, tag, attrs):
+    global start_tags
+    start_tags.append(tag)
+    # method to append the end tag to the list end_tags.
+  def handle_endtag(self, tag):
+    global end_tags
+    end_tags.append(tag)
+  # method to append the data between the tags to the list all_data.
+  def handle_data(self, data):
+    global all_data
+    all_data.append(data)
+  # method to append the comment to the list comments.
+  def handle_comment(self, data):
+    global comments
+    comments.append(data)
+start_tags = []
+end_tags = []
+all_data = []
+comments = []
+# Creating an instance of our class.
+html_parser = Parser()
+html_parser.feed("".join(DOM_structure))
+print(start_tags)
+print(end_tags)
+print(all_data)
+print(comments)
